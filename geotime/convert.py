a80c5e6a9e25136c3478ff396baf414eea4f8456
Eli Talbert <etalbert102@gmail.com>
2024-05-02 17:10:52 -0400

    more mods

--- geotime/convert.py
+++ geotime/convert.py
@@ -1,22 +1,57 @@
 """ Conversion from FeatureCollection to geotime datastructure """
 from typing import String
-from geostructures.collection import Track
+from geostructures.collection import  FeatureCollection, Track
 from geotime.chronnet import chronnet_create
 from geotime.timecube import TimeCube
+from datetime import datetime, timedelta
 
-def convert(collection: Track, datastructure: String):
+def time_slice_track(track: Track, hour_interval: int):
     """
-    Converts a geostructures Track to a desired geotime datastructure
+    Slices a Track into several tracks that are partitioned by a specified length
+    of time in hours  
     
     Args:
-        collection: the target FeatureCollection for conversion
+        track: the target Track
+
+        hour_interval= the length in hours of the desired interval
+
+    Returns:
+        A list of sliced tracks
+    """
+    start_time = track.start
+    end_time = track.end
+    timestamps = []
+    sliced_tracks = []
+
+    # get timestamp intervals
+    date_x = start_time
+    while date_x < end_time:
+        date_x += timedelta(hours = hour_interval)
+        timestamps.append(date_x)
+    
+    
+    for timestamp in timestamps:
+        sliced_track = track[start_time: timestamp]
+        start_time = timestamp
+        sliced_tracks.append(sliced_track)
+    
+    return sliced_tracks
+
+def convert(fcol: FeatureCollection, datastructure: String):
+    """
+    Converts a geostructures FeatureCollection to a desired geotime datastructure
+    
+    Args:
+        fcol: the target FeatureCollection
 
         datastructure: the desired datastructure currently timecube, and
         chronnet are supported
 
     Returns:
         Desired geotime representation
-    """ 
+    """
+    track = Track(fcol.geoshapes)
+
     if datastructure == "timecube":
         result = 
     elif datastructure == "chronnet":
