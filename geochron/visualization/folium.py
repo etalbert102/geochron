bca6e964e3683cfe10ac12b85b592bc516328a79
Eli Talbert <etalbert102@gmail.com>
2024-06-04 12:54:06 -0400

    implemented timestamped geojsonviz

--- geochron/visualization/folium.py
+++ geochron/visualization/folium.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 from branca.colormap import LinearColormap 
 from typing import Callable, Optional
+from geostructures.geohash import h3_to_geopolygon
+from geostructures import FeatureCollection
 
 def timehex_styledict(timehex: pd.DataFrame, opacity= float(.7), cmap:Optional[Callable] = None):
     """
@@ -33,15 +35,14 @@ def timehex_styledict(timehex: pd.DataFrame, opacity= float(.7), cmap:Optional[C
     else:
         used_cmap = cmap
 
-    styledict = {
-    str(id): {
-        time: {'color': used_cmap(color), 'opacity': 0 if color == 0 else opacity} 
-        for time, color in zip(timehex_long[timehex_long['id'] == id]['time'], timehex_long[timehex_long['id'] == id]['value'])
-    } 
-    for id in timehex_long['id'].unique()
-    }
+
+    # Vectorized operations for creating styledict
+    styledict = timehex_long.groupby('id').apply(lambda df: df.set_index('time')['value']\
+    .apply(lambda x: {'color': used_cmap(x), 'opacity': 0 if x == 0 else opacity}).to_dict()).to_dict()
 
     return styledict
+
+
 
 def h3_to_geojson(h3_hashmap):
     import h3
@@ -82,3 +83,35 @@ def timehex_backgroundata(timehex: pd.DataFrame):
     backgroundata = h3_to_geojson(hashmap)
 
     return backgroundata
+
+def add_hashmap_properties(original_hashmap, time, cmap: Callable, opacity= float(.7)):
+    import h3
+    new_dict = {}
+    removed_empty = {key: value for key, value in original_hashmap.items() if value != 0}
+    for key, value in removed_empty.items():
+        new_dict[key] = {'popup': 'weight= ' + str(value) +
+                         '<br> center(lat,lon)= ' + str(h3.h3_to_geo(key)), 
+                         'time': time,'style':{'opacity': opacity, 'color': cmap(value)}}
+    return new_dict
+
+def timehex_timestampedgeojson(timehex: pd.DataFrame, cmap:Optional[Callable] = None):
+    start_time_list = timehex['start_time'].tolist()
+    select_timehex = timehex.drop(['interval', 'start_time', 'end_time'], axis=1).fillna(0)
+    list_hashmaps = select_timehex.to_dict('records')
+    polygon_list = []
+
+    #color scale 
+    max_color = select_timehex.values.max()
+    min_color = select_timehex.values.min()
+    if cmap == None:
+        used_cmap = LinearColormap(colors=['blue','red'], vmin=min_color, vmax=max_color)
+    else:
+        used_cmap = cmap
+    
+    for hashmap, start_time in zip(list_hashmaps, start_time_list):
+        hashmap_properties= add_hashmap_properties(hashmap, start_time, used_cmap)
+        polygons = {h3_to_geopolygon(k, properties= v) for k,v in hashmap_properties.items()}
+        polygon_list.extend(polygons)
+    
+    return FeatureCollection(polygon_list).to_geojson()
+        
